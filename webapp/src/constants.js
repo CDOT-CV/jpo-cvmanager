21224a3d2b2890e3671dcd790030e0eaa97e1e7c
Michael7371 <40476797+Michael7371@users.noreply.github.com>
2023-07-27 10:07:17 -0600

    updated constants message types

--- webapp/src/constants.js
+++ webapp/src/constants.js
@@ -1,5 +1,5 @@
-export const MessageTypes = ['BSM', 'SSM', 'SPAT', 'SRM', 'MAP']
-export const DotName = 'CDOT'
+export const MessageTypes = ['BSM', 'PSM', 'SPAT', 'TIM', 'MAP']
+export const DotName = 'NJDOT'
 export const MapboxInitViewState = {
   latitude: 40.2712,
   longitude: -74.7809,
