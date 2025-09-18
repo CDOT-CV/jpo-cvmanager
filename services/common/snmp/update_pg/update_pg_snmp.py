81286f5502b235c869eb4b79d29b8c3d11a6e3dc
dmccoystephenson <dmccoystephenson@gmail.com>
2025-09-18 10:47:58 -0600

    refactor: update get_rsu_list to use configurable schema name in SQL query

--- services/common/snmp/update_pg/update_pg_snmp.py
+++ services/common/snmp/update_pg/update_pg_snmp.py
@@ -1,17 +1,19 @@
 from abc import ABC, abstractmethod
+import os
 import common.pgquery as pgquery
 
 
 # An interface for custom scripts that update SNMP configurations in the PostgreSQL database
 class UpdatePostgresSnmpAbstractClass(ABC):
     def get_rsu_list(self):
+        schema_name = os.getenv("POSTGRES_SCHEMA_NAME", "public")
         query = (
             "SELECT to_jsonb(row) "
             "FROM ("
             "SELECT rd.rsu_id, rd.ipv4_address, snmp.username AS snmp_username, snmp.password AS snmp_password, snmp.encrypt_password AS snmp_encrypt_pw, sver.protocol_code AS snmp_version "
-            "FROM public.rsus AS rd "
-            "LEFT JOIN public.snmp_credentials AS snmp ON snmp.snmp_credential_id = rd.snmp_credential_id "
-            "LEFT JOIN public.snmp_protocols AS sver ON sver.snmp_protocol_id = rd.snmp_protocol_id"
+            "FROM {schema_name}.rsus AS rd "
+            "LEFT JOIN {schema_name}.snmp_credentials AS snmp ON snmp.snmp_credential_id = rd.snmp_credential_id "
+            "LEFT JOIN {schema_name}.snmp_protocols AS sver ON sver.snmp_protocol_id = rd.snmp_protocol_id"
             ") as row"
         )
 
