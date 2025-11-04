0930867836f7ea2f115cb2eeb9307b2b72c55ff3
jacob6838 <jfrye@neaeraconsulting.com>
2025-11-03 17:41:40 -0700

    Fully generating ProcessedSrm and ProcessedSsm

--- webapp/src/models/imports/ProcessedSrm.d.ts
+++ webapp/src/models/imports/ProcessedSrm.d.ts
@@ -10,9 +10,152 @@ type ProcessedSrmFeature = {
 type ProcessedSrmProperties = {
   schemaVersion: number
   messageType: 'SRM'
-  asn1: string
   odeReceivedAt: string
   odeReceivedAtEpochSeconds: number
-  timeStamp: string
   originIp: string
+  asn1: string
+  timeStamp: string
+  sequenceNumber?: number
+  vehicleID?: string
+  role?: ProcessedBasicVehicleRole
+  subrole?: ProcessedRequestSubRole
+  importanceLevel?: ProcessedRequestImportanceLevel
+  iso3833VehicleType?: number
+  hpmsType?: ProcessedVehicleType
+  latitude?: number
+  longitude?: number
+  elevation?: number
+  heading?: number
+  transmission?: ProcessedTransmissionState
+  speedMetersPerSecond?: number
+  name?: string
+  routeName?: string
+  transitStatus?: ProcessedTransitVehicleStatus
+  transitOccupancy?: ProcessedTransitVehicleOccupancy
+  transitScheduleSeconds?: number // Duration in seconds
+  requests?: ProcessedSignalRequest[]
+  validationMessages: ProcessedValidationMessage[]
 }
+
+type ProcessedSignalRequest = {
+  region?: number
+  intersectionId?: number
+  requestID?: number
+  priorityRequestType?: ProcessedPriorityRequestType
+  inboundLaneID?: number
+  inboundApproachID?: number
+  inboundLaneConnectionID?: number
+  outboundLaneID?: number
+  outboundApproachID?: number
+  outboundLaneConnectionID?: number
+  estimatedTimeOfArrival?: string
+  estimatedTimeOfArrivalDurationSeconds?: number // Duration in seconds
+}
+
+// Enum-like types
+type ProcessedBasicVehicleRole =
+  | 'basicVehicle'
+  | 'publicTransport'
+  | 'specialTransport'
+  | 'dangerousGoods'
+  | 'roadWork'
+  | 'roadRescue'
+  | 'emergency'
+  | 'safetyCar'
+  | 'none-unknown'
+  | 'truck'
+  | 'motorcycle'
+  | 'roadSideSource'
+  | 'police'
+  | 'fire'
+  | 'ambulance'
+  | 'dot'
+  | 'transit'
+  | 'slowMoving'
+  | 'stopNgo'
+  | 'cyclist'
+  | 'pedestrian'
+  | 'nonMotorized'
+  | 'military'
+
+type ProcessedRequestSubRole =
+  | 'requestSubRoleUnKnown'
+  | 'requestSubRole1'
+  | 'requestSubRole2'
+  | 'requestSubRole3'
+  | 'requestSubRole4'
+  | 'requestSubRole5'
+  | 'requestSubRole6'
+  | 'requestSubRole7'
+  | 'requestSubRole8'
+  | 'requestSubRole9'
+  | 'requestSubRole10'
+  | 'requestSubRole11'
+  | 'requestSubRole12'
+  | 'requestSubRole13'
+  | 'requestSubRole14'
+  | 'requestSubRoleReserved'
+
+type ProcessedRequestImportanceLevel =
+  | 'requestImportanceLevelUnKnown'
+  | 'requestImportanceLevel1'
+  | 'requestImportanceLevel2'
+  | 'requestImportanceLevel3'
+  | 'requestImportanceLevel4'
+  | 'requestImportanceLevel5'
+  | 'requestImportanceLevel6'
+  | 'requestImportanceLevel7'
+  | 'requestImportanceLevel8'
+  | 'requestImportanceLevel9'
+  | 'requestImportanceLevel10'
+  | 'requestImportanceLevel11'
+  | 'requestImportanceLevel12'
+  | 'requestImportanceLevel13'
+  | 'requestImportanceLevel14'
+  | 'requestImportanceReserved'
+
+type ProcessedVehicleType =
+  | 'none'
+  | 'unknown'
+  | 'special'
+  | 'moto'
+  | 'car'
+  | 'carOther'
+  | 'bus'
+  | 'axleCnt2'
+  | 'axleCnt3'
+  | 'axleCnt4'
+  | 'axleCnt4Trailer'
+  | 'axleCnt5Trailer'
+  | 'axleCnt6Trailer'
+  | 'axleCnt5MultiTrailer'
+  | 'axleCnt6MultiTrailer'
+  | 'axleCnt7MultiTrailer'
+
+type ProcessedTransmissionState =
+  | 'neutral'
+  | 'park'
+  | 'forwardGears'
+  | 'reverseGears'
+  | 'reserved1'
+  | 'reserved2'
+  | 'reserved3'
+  | 'unavailable'
+
+type ProcessedTransitVehicleStatus = 'loading' | 'anADAuse' | 'aBikeLoad' | 'doorOpen' | 'charging' | 'atStopLine'
+
+type ProcessedTransitVehicleOccupancy =
+  | 'occupancyUnknown'
+  | 'occupancyEmpty'
+  | 'occupancyVeryLow'
+  | 'occupancyLow'
+  | 'occupancyMed'
+  | 'occupancyHigh'
+  | 'occupancyNearlyFull'
+  | 'occupancyFull'
+
+type ProcessedPriorityRequestType =
+  | 'priorityRequestTypeReserved'
+  | 'priorityRequest'
+  | 'priorityRequestUpdate'
+  | 'priorityCancellation'
--- webapp/src/models/imports/ProcessedSsm.d.ts
+++ webapp/src/models/imports/ProcessedSsm.d.ts
@@ -6,4 +6,41 @@ type ProcessedSsm = {
   odeReceivedAtEpochSeconds: number
   originIp: string
   timeStamp: string
+  sequenceNumber?: number
+  statusSequenceNumber?: number
+  region?: number
+  intersectionId?: number
+  statusList?: ProcessedSignalStatus[]
+  validationMessages: ProcessedValidationMessage[]
 }
+
+type ProcessedSignalStatus = {
+  vehicleID?: string
+  requestID?: number
+  requesterSequenceNumber?: number
+  requesterRole?: ProcessedBasicVehicleRole
+  requesterSubrole?: ProcessedRequestSubRole
+  requestImportanceLevel?: ProcessedRequestImportanceLevel
+  requesterIso3833VehicleType?: number
+  requesterHpmsType?: ProcessedVehicleType
+  inboundOnLaneID?: number
+  inboundOnApproachID?: number
+  inboundOnLaneConnectionID?: number
+  outboundOnLaneID?: number
+  outboundOnApproachID?: number
+  outboundOnLaneConnectionID?: number
+  estimatedTimeOfArrival?: string
+  estimatedTimeOfArrivalDurationSeconds?: number // Duration in seconds
+  status?: ProcessedPrioritizationResponseStatus
+}
+
+// New enum type for SSM
+type ProcessedPrioritizationResponseStatus =
+  | 'unknown'
+  | 'requested'
+  | 'processing'
+  | 'watchOtherTraffic'
+  | 'granted'
+  | 'rejected'
+  | 'maxPresence'
+  | 'reserviceLocked'
