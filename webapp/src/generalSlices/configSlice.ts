35007f7e1685c9d65ede15870883f4c1c657527a
dmccoystephenson <dmccoystephenson@gmail.com>
2026-01-13 15:23:07 -0700

    fix: add rejectWithValue for improved error handling in geoRsuQuery

--- webapp/src/generalSlices/configSlice.ts
+++ webapp/src/generalSlices/configSlice.ts
@@ -214,7 +214,7 @@ export const startFirmwareUpgrade = createAsyncThunk(
 
 export const geoRsuQuery = createAsyncThunk(
   'config/geoRsuQuery',
-  async (vendor: string, { getState }) => {
+  async (vendor: string, { getState, rejectWithValue }) => {
     const currentState = getState() as RootState
     const token = selectToken(currentState)
     const organization = selectOrganizationName(currentState)
