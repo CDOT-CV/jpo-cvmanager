5d69fadf2de6399662e78405fe02d7c2b4def14b
dmccoystephenson <dmccoystephenson@gmail.com>
2026-01-27 11:55:03 -0700

    chore: set CI timezone to UTC, switch to `npm ci`, and enhance `DateTime.local` override in tests

--- webapp/src/setupTests.ts
+++ webapp/src/setupTests.ts
@@ -36,7 +36,12 @@ vi.mock('luxon', async () => {
     ...actualLuxon,
     DateTime: {
       ...actualLuxon.DateTime,
-      local: () => actualLuxon.DateTime.fromISO('2024-04-10T00:00:00.000+00:00').setZone('America/Denver'),
+      local: (...args: any[]) => {
+        if (args.length > 0) {
+          return actualLuxon.DateTime.local(...args)
+        }
+        return actualLuxon.DateTime.fromISO('2024-04-10T00:00:00.000+00:00').setZone('America/Denver')
+      },
       fromISO: actualLuxon.DateTime.fromISO,
       fromJSDate: actualLuxon.DateTime.fromJSDate,
     },
