afba2dbf23bb2a362e2b9b9bb3441d7f963685c4
dmccoystephenson <dmccoystephenson@gmail.com>
2026-01-29 14:32:49 -0700

    db: update comment in rsu_options.sql

--- resources/sql_scripts/update_scripts/rsu_options.sql
+++ resources/sql_scripts/update_scripts/rsu_options.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS public.rsu_options (
 );
 
 -- Populate existing rows for tim_deposit
--- Populate with '0' UNLESS they belong to the "Region 1" organization
+-- Populate with 'TRUE' UNLESS they belong to the "Region 1" organization
 INSERT INTO public.rsu_options (rsu_id, tim_deposit, snmp_monitoring)
 SELECT rsu_id, TRUE, FALSE FROM public.rsus
 ON CONFLICT (rsu_id) DO NOTHING;
