3304f0f6751a197582aab8ebd1cb2bd65a0ad37a
dmccoystephenson <dmccoystephenson@gmail.com>
2026-02-18 09:43:01 -0700

    refactor(api): remove AdminRsu endpoint and related functionality

--- services/api/src/main.py
+++ services/api/src/main.py
@@ -21,7 +21,6 @@ from rsu_geo_msg_query import RsuGeoData
 from iss_scms_status import IssScmsStatus
 from rsu_ssm_srm import RsuSsmSrmData
 from admin_new_rsu import AdminNewRsu
-from admin_rsu import AdminRsu
 from admin_new_intersection import AdminNewIntersection
 from admin_intersection import AdminIntersection
 from admin_new_user import AdminNewUser
@@ -77,7 +76,6 @@ if api_environment.ENABLE_RSU_FEATURES:
     api.add_resource(IssScmsStatus, "/iss-scms-status")
     api.add_resource(RsuSsmSrmData, "/rsu-ssm-srm-data")
     api.add_resource(AdminNewRsu, "/admin-new-rsu")
-    api.add_resource(AdminRsu, "/admin-rsu")
     api.add_resource(RSUErrorSummaryResource, "/rsu-error-summary")
 if api_environment.ENABLE_WZDX_FEATURES:
     api.add_resource(WzdxFeed, "/wzdx-feed")
