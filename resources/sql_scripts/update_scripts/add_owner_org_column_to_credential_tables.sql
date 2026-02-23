ea59ff1bbe3ab6d5ad8392058120aa7014b389e9
dmccoystephenson <dmccoystephenson@gmail.com>
2026-02-23 10:31:47 -0700

    sql: refactor owner_organization_id updates in migration script for credential tables

--- resources/sql_scripts/update_scripts/add_owner_org_column_to_credential_tables.sql
+++ resources/sql_scripts/update_scripts/add_owner_org_column_to_credential_tables.sql
@@ -4,20 +4,15 @@ ALTER TABLE public.rsu_credentials
     ADD CONSTRAINT fk_owner_organization_id FOREIGN KEY (owner_organization_id)
         REFERENCES public.organizations (organization_id);
 
-DO
-$do$
-    BEGIN
-        FOR i in 1..(SELECT count(*) FROM public.rsu_credentials)
-            LOOP
-                UPDATE public.rsu_credentials
-                SET owner_organization_id = (SELECT organization_id
-                                             FROM public.rsu_organization
-                                             WHERE rsu_id IN
-                                                   (SELECT rsu_id FROM public.rsus WHERE credential_id = i LIMIT 1))
-                where credential_id = i;
-            END LOOP;
-    END
-$do$;
+UPDATE public.rsu_credentials rc
+SET owner_organization_id = (
+    SELECT ro.organization_id
+    FROM public.rsu_organization ro
+    JOIN public.rsus r ON ro.rsu_id = r.rsu_id
+    WHERE r.credential_id = rc.credential_id
+    ORDER BY r.rsu_id ASC
+    LIMIT 1
+);
 
 ALTER TABLE public.rsu_credentials
     ALTER COLUMN owner_organization_id SET NOT NULL;
@@ -29,20 +24,15 @@ ALTER TABLE public.snmp_credentials
     ADD CONSTRAINT fk_owner_organization_id FOREIGN KEY (owner_organization_id)
         REFERENCES public.organizations (organization_id);
 
-DO
-$do$
-    BEGIN
-        FOR i in 1..(SELECT count(*) FROM public.snmp_credentials)
-            LOOP
-                UPDATE public.snmp_credentials
-                SET owner_organization_id = (SELECT organization_id
-                                             FROM public.rsu_organization
-                                             WHERE rsu_id IN
-                                                   (SELECT rsu_id FROM public.rsus WHERE snmp_credential_id = i LIMIT 1))
-                where snmp_credential_id = i;
-            END LOOP;
-    END
-$do$;
+UPDATE public.snmp_credentials sc
+SET owner_organization_id = (
+    SELECT ro.organization_id
+    FROM public.rsu_organization ro
+    JOIN public.rsus r ON ro.rsu_id = r.rsu_id
+    WHERE r.snmp_credential_id = sc.snmp_credential_id
+    ORDER BY r.rsu_id ASC
+    LIMIT 1
+);
 
 ALTER TABLE public.snmp_credentials
     ALTER COLUMN owner_organization_id SET NOT NULL;
