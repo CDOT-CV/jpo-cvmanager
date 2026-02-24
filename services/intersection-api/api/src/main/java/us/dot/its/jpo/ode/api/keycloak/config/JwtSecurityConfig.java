a066e487b45390290f87283a02a3c77afc885a69
Paige Monington <pmonington@trihydro.com>
2026-02-24 10:01:13 -0700

    chore: adjust oauth2 package to new location

--- services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/keycloak/config/JwtSecurityConfig.java
+++ services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/keycloak/config/JwtSecurityConfig.java
@@ -1,7 +1,7 @@
 package us.dot.its.jpo.ode.api.keycloak.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.security.autoconfigure.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.boot.security.oauth2.server.resource.autoconfigure.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
