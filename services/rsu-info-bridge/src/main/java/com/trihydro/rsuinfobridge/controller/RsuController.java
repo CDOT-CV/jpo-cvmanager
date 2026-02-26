b9fdcabfd7a58d584bc6d23cd414c914350a7640
dmccoystephenson <dmccoystephenson@gmail.com>
2026-02-26 16:44:45 -0700

    refactor: remove POST endpoint and related test in RsuController

--- services/rsu-info-bridge/src/main/java/com/trihydro/rsuinfobridge/controller/RsuController.java
+++ services/rsu-info-bridge/src/main/java/com/trihydro/rsuinfobridge/controller/RsuController.java
@@ -3,10 +3,7 @@ package com.trihydro.rsuinfobridge.controller;
 import com.trihydro.rsuinfobridge.models.dtos.RsuDto;
 import com.trihydro.rsuinfobridge.service.RsuService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,10 +19,5 @@ public class RsuController {
     @GetMapping
     public List<RsuDto> getAll(@RequestParam(defaultValue = "false") boolean timDepositEnabled) {
         return rsuService.getAll(timDepositEnabled);
-    }
-
-    @PostMapping("/query")
-    public List<RsuDto> getRsusByIds(@RequestBody List<Long> rsuIds) {
-        return rsuService.getByIds(rsuIds);
     }
 }
