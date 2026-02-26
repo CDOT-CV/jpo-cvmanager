b348a0f2ecfa238feba15175a5080b562c4c7180
dmccoystephenson <dmccoystephenson@gmail.com>
2026-02-26 12:01:03 -0700

    refactor: remove redundant @JsonProperty annotations from DTO classes

--- services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/models/credentials/SnmpCredentialDTO.java
+++ services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/models/credentials/SnmpCredentialDTO.java
@@ -1,6 +1,5 @@
 package us.dot.its.jpo.ode.api.models.credentials;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -14,18 +13,13 @@ import lombok.Setter;
 @EqualsAndHashCode
 public class SnmpCredentialDTO {
 
-    @JsonProperty
     private Integer id;
 
-    @JsonProperty
     private String nickname;
 
-    @JsonProperty
     private String username;
 
-    @JsonProperty
     private String password;
 
-    @JsonProperty
     private Integer ownerOrganizationId;
 }
