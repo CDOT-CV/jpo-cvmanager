3a9b2dbff9a3d99f2a957e493d08fe5915166173
dmccoystephenson <dmccoystephenson@gmail.com>
2026-02-27 11:48:17 -0700

    docs: add description for timDepositEnabled parameter in getAll endpoint

--- services/rsu-info-bridge/src/main/java/com/trihydro/rsuinfobridge/controller/RsuController.java
+++ services/rsu-info-bridge/src/main/java/com/trihydro/rsuinfobridge/controller/RsuController.java
@@ -3,6 +3,7 @@ package com.trihydro.rsuinfobridge.controller;
 import com.trihydro.rsuinfobridge.models.dtos.RsuDto;
 import com.trihydro.rsuinfobridge.service.RsuService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -26,7 +27,9 @@ public class RsuController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successfully retrieved list of RSUs")
     })
-    public List<RsuDto> getAll(@RequestParam(defaultValue = "false") boolean timDepositEnabled) {
+    public List<RsuDto> getAll(
+            @Parameter(description = "Filter RSUs by TIM deposit enabled status", example = "false")
+            @RequestParam(defaultValue = "false") boolean timDepositEnabled) {
         return rsuService.getAll(timDepositEnabled);
     }
 }
