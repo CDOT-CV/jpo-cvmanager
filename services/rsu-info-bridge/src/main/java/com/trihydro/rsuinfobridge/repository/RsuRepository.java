16a83b11e737f0c285c0f877f3480b8a23ad307c
dmccoystephenson <dmccoystephenson@gmail.com>
2026-03-02 09:43:49 -0700

    chore: add custom query method to RsuRepository for filtering by TimDeposit

--- services/rsu-info-bridge/src/main/java/com/trihydro/rsuinfobridge/repository/RsuRepository.java
+++ services/rsu-info-bridge/src/main/java/com/trihydro/rsuinfobridge/repository/RsuRepository.java
@@ -1,10 +1,14 @@
 package com.trihydro.rsuinfobridge.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.trihydro.rsuinfobridge.models.tables.Rsu;
 
+import java.util.List;
+
 @Repository
 public interface RsuRepository extends JpaRepository<Rsu, Integer> {
-    // TODO: add necessary methods
+    @Query("SELECT rsu FROM Rsu rsu WHERE rsu.rsuOption.timDeposit = :rsuOptionTimDeposit")
+    List<Rsu> findByRsuOption_TimDeposit(Boolean rsuOptionTimDeposit);
 }
