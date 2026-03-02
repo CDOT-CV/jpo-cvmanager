e5db82ebd937c5013beb3987e630429542e2c976
dmccoystephenson <dmccoystephenson@gmail.com>
2026-03-02 13:31:09 -0700

    test: add embedded PostgreSQL dependency for integration tests and clean up imports in RsuInfoBridgeApplicationTests

--- services/rsu-info-bridge/src/test/java/com/trihydro/rsuinfobridge/RsuInfoBridgeApplicationTests.java
+++ services/rsu-info-bridge/src/test/java/com/trihydro/rsuinfobridge/RsuInfoBridgeApplicationTests.java
@@ -3,7 +3,6 @@ package com.trihydro.rsuinfobridge;
 import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
