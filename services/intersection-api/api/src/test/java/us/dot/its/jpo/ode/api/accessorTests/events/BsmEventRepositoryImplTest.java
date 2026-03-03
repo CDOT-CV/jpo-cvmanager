7ee1eb76b0e01dde59f15cd20ac3a0bf09a74b60
Paige Monington <pmonington@trihydro.com>
2026-03-03 10:48:39 -0700

    test: add missing @SpringBootTest annotation in BsmEventRepositoryImplTest

--- services/intersection-api/api/src/test/java/us/dot/its/jpo/ode/api/accessorTests/events/BsmEventRepositoryImplTest.java
+++ services/intersection-api/api/src/test/java/us/dot/its/jpo/ode/api/accessorTests/events/BsmEventRepositoryImplTest.java
@@ -9,6 +9,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
