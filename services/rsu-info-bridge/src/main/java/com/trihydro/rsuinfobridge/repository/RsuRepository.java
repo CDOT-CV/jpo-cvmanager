48f1cc5fac0ddc2cdf59d885ee597c816ba2d3b1
dmccoystephenson <dmccoystephenson@gmail.com>
2026-03-03 15:25:51 -0700

    chore: remove unused import statement from RsuRepository to clean up code

--- services/rsu-info-bridge/src/main/java/com/trihydro/rsuinfobridge/repository/RsuRepository.java
+++ services/rsu-info-bridge/src/main/java/com/trihydro/rsuinfobridge/repository/RsuRepository.java
@@ -1,7 +1,6 @@
 package com.trihydro.rsuinfobridge.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.trihydro.rsuinfobridge.models.tables.Rsu;
 
