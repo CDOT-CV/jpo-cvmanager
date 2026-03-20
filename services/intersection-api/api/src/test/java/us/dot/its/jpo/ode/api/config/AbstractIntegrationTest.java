eafb79f599f36746c9209eac3de4cb931ce987fa
Matt Cook <matt.cook@trihydro.com>
2026-03-20 16:19:49 -0600

    feat(tests): add integration test profile for Intersection API
    - Introduced `application-integration-test.yaml` for service-layer integration tests.
    - Configured test containers for PostgreSQL using `tc:` JDBC URL and MongoDB via `@DynamicPropertySource`.
    - Defined Kafka topics and schemas, security, email, and other environment-specific settings for testing.

--- services/intersection-api/api/src/test/java/us/dot/its/jpo/ode/api/config/AbstractIntegrationTest.java
+++ services/intersection-api/api/src/test/java/us/dot/its/jpo/ode/api/config/AbstractIntegrationTest.java
@@ -4,11 +4,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
