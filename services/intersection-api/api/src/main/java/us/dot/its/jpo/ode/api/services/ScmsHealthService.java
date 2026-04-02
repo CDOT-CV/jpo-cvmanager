7b2420a1149bb6b82f0cfabb4caa33e3e19fd3a6
dmccoystephenson <dmccoystephenson@gmail.com>
2026-04-02 16:02:17 -0600

    chore: remove unused Lombok @Slf4j annotation from ScmsHealthService

--- services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/services/ScmsHealthService.java
+++ services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/services/ScmsHealthService.java
@@ -1,7 +1,6 @@
 package us.dot.its.jpo.ode.api.services;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import us.dot.its.jpo.ode.api.models.postgres.projections.ScmsHealthRsuProjection;
 import us.dot.its.jpo.ode.api.repositories.ScmsHealthRepository;
@@ -10,7 +9,6 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class ScmsHealthService {
     private final ScmsHealthRepository scmsHealthRepository;
 
