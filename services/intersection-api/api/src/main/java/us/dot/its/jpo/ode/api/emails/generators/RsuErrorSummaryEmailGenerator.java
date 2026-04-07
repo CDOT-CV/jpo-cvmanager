659a5a8f60f480cf9ff75008e196424916cc9f80
jacob6838 <jfrye@neaeraconsulting.com>
2026-04-07 16:42:34 -0600

    Reverting unnecessary changes

--- services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/emails/generators/RsuErrorSummaryEmailGenerator.java
+++ services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/emails/generators/RsuErrorSummaryEmailGenerator.java
@@ -23,7 +23,8 @@ public class RsuErrorSummaryEmailGenerator extends AbstractEmailGenerator<RsuErr
 
         Context context = this.generateEmailContextBasic();
         context.setVariable("preview_text", "RSU Error Summary from CV Manager");
-        context.setVariable("content_1", escapeHtml(data.getMessage()));
+        context.setVariable("content_1", data.getMessage());
+        // NOTE: Not escaping HTML in message because message is in HTML format
 
         String htmlContent = templateEngine.process("emails/email_template_no_unsubscribe", context);
 
