fbad6e4cd65cff978b52404c0e4312a38ea3a43d
Matt Cook <matt.cook@trihydro.com>
2026-04-07 16:59:51 -0600

    refactor(webapp): remove unused types and simplify tests in AdminOrgTabIntersection
    - Deleted unused types (`AdminOrgIntersectionWithId` and `AdminOrgTabIntersectionAddMultiple`) for clean-up.
    - Removed redundant selectors, functions, and reducer tests.
    - Refactored tests to align with updated RTK Query integration, including mock adjustments and streamlined assertions.

--- webapp/src/features/adminOrganizationTabIntersection/AdminOrganizationTabIntersectionTypes.d.ts
+++ webapp/src/features/adminOrganizationTabIntersection/AdminOrganizationTabIntersectionTypes.d.ts
@@ -1,16 +1,5 @@
 import { AdminOrgIntersection } from '../adminOrganizationTab/adminOrganizationTabSlice'
 
-export type AdminOrgIntersectionWithId = AdminOrgIntersection & {
-  id?: number
-}
-
-export type AdminOrgTabIntersectionAddMultiple = {
-  intersectionList: AdminOrgIntersection[]
-  selectedOrg: string
-  selectedOrgEmail: string
-  updateTableData: (org: string) => void
-}
-
 export type AdminOrgIntersectionDeleteSingle = {
   intersection: AdminOrgIntersection
   selectedOrg: string
