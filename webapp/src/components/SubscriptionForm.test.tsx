4850dc68832d0c8ca46b26edebc8a0b4786d1048
jacob6838 <jfrye@neaeraconsulting.com>
2026-04-21 12:12:07 -0600

    Update SubscriptionForm.test.tsx

--- webapp/src/components/SubscriptionForm.test.tsx
+++ webapp/src/components/SubscriptionForm.test.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react'
 import SubscriptionForm from './SubscriptionForm'
 import { replaceChaoticIds } from '../utils/test-utils'
+import { BrowserRouter } from 'react-router-dom'
 
 it('should take a snapshot', () => {
   const { container } = render(
