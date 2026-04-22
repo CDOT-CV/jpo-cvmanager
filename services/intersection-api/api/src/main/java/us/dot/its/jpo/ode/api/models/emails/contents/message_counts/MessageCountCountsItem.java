e1e00d0f9a4cda7a6cb5e477d27335d50662f5f7
jacob6838 <jfrye@neaeraconsulting.com>
2026-04-22 16:54:35 -0600

    Updating diff_percent schema definition

--- services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/models/emails/contents/message_counts/MessageCountCountsItem.java
+++ services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/models/emails/contents/message_counts/MessageCountCountsItem.java
@@ -12,7 +12,11 @@ public class MessageCountCountsItem {
     private int in;
     @Schema(description = "Egress message counts")
     private int out;
-    @Schema(description = "Percentage difference between ingress and egress message counts")
+    @Schema(description = "Percentage difference between egress and ingress message counts " +
+            "(absolute value, always positive), where 0% difference means identical counts. For commonly " +
+            "deduplicated message types (like MAP), the percent difference accounts for expected 3600:1 " +
+            "deduplication. In cases where the in counts are zero or the out counts are greater than the " +
+            "in counts, the diff percent is set to 6%, enough to trigger an error (>5%)")
     @JsonProperty("diff_percent")
     private double diffPercent;
 }
