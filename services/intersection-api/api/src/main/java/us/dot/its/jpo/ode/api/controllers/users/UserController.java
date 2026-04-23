3f22c87f0ec1d29f0add41472c4b1f22c5aa8cd8
jacob6838 <jfrye@neaeraconsulting.com>
2026-04-23 14:12:50 -0600

    Using ResponseStatus Created in createUser controller

--- services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/controllers/users/UserController.java
+++ services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/controllers/users/UserController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -118,8 +119,7 @@ public class UserController {
         }
 
         userManagementService.createUser(body);
-
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return;
     }
 
     @Operation(summary = "Modify User", description = "Modify User information")
