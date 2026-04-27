d00fe7e97c93f42c6c9fb7304e5ea901b3f5e7ff
jacob6838 <jfrye@neaeraconsulting.com>
2026-04-27 15:01:56 -0600

    Reverting AdminIntersectionTab test changes

--- webapp/src/features/adminIntersectionTab/AdminIntersectionTab.test.tsx
+++ webapp/src/features/adminIntersectionTab/AdminIntersectionTab.test.tsx
@@ -7,42 +7,11 @@ import { setupStore } from '../../store'
 import { replaceChaoticIds } from '../../utils/test-utils'
 import { BrowserRouter } from 'react-router-dom'
 
-const mockTableData = [
-  {
-    intersection_id: 'int-1',
-    intersection_name: 'Test Intersection 1',
-    origin_ip: '192.168.1.1',
-    rsus: 'RSU-001, RSU-002',
-  },
-  {
-    intersection_id: 'int-2',
-    intersection_name: 'Test Intersection 2',
-    origin_ip: '192.168.1.2',
-    rsus: 'RSU-003',
-  },
-]
-
 test('should take a snapshot', () => {
   const { container } = render(
     <ThemeProvider theme={testTheme}>
       <Provider
-        store={setupStore({
-          adminIntersectionTab: {
-            loading: false,
-            value: {
-              activeDiv: 'intersection_table',
-              tableData: mockTableData,
-              title: 'Intersections',
-              columns: [
-                { title: 'Intersection ID', field: 'intersection_id', id: 0 },
-                { title: 'Intersection Name', field: 'intersection_name', id: 1 },
-                { title: 'Origin IP', field: 'origin_ip', id: 2 },
-                { title: 'Linked RSUs', field: 'rsus', id: 3 },
-              ],
-              editIntersectionRowData: {},
-            },
-          },
-        })}
+        store={setupStore({ adminIntersectionTab: { loading: false, value: { activeDiv: 'intersection_table' } } })}
       >
         <BrowserRouter>
           <AdminIntersectionTab />
