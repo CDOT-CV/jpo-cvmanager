189180e8ba548f3dacca216801627b5549ac96f6
jacob6838 <jfrye@neaeraconsulting.com>
2026-05-11 11:17:58 -0600

    Fixing IAPI swagger docs page

--- services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/WebConfig.java
+++ services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/WebConfig.java
@@ -3,7 +3,9 @@ package us.dot.its.jpo.ode.api;
 import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,6 +19,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new ByteArrayHttpMessageConverter());
+        converters.add(new StringHttpMessageConverter());
         converters.add(new MappingJackson2HttpMessageConverter(DateJsonMapper.getInstance()));
     }
 }
