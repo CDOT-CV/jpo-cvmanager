52082750e85956c61ae2ab0e98325bdbeb1c1b3b
jacob6838 <jfrye@neaeraconsulting.com>
2026-05-18 16:58:55 -0600

    Removing unused methods/classes

--- services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/models/postgres/tables/UserEmailNotification.java
+++ services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/models/postgres/tables/UserEmailNotification.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
-import us.dot.its.jpo.ode.api.models.emails.UserEmailNotificationDto;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.OnDelete;
@@ -57,28 +56,4 @@ public class UserEmailNotification {
     @ColumnDefault("false")
     @Column(name = "monthly", nullable = false)
     private Boolean monthly;
-
-    public Boolean getSubscribed() {
-        return (immediate != null && immediate) ||
-                (hourly != null && hourly) ||
-                (daily != null && daily) ||
-                (weekly != null && weekly) ||
-                (monthly != null && monthly);
-    }
-
-    public Boolean isFrequencyEqual(UserEmailNotificationDto other) {
-        return (this.immediate != null && other.immediate != null && this.immediate.equals(other.immediate))
-                && (this.hourly != null && other.hourly != null && this.hourly.equals(other.hourly))
-                && (this.daily != null && other.daily != null && this.daily.equals(other.daily))
-                && (this.weekly != null && other.weekly != null && this.weekly.equals(other.weekly))
-                && (this.monthly != null && other.monthly != null && this.monthly.equals(other.monthly));
-    }
-
-    public void updateFrequency(UserEmailNotificationDto dto) {
-        this.immediate = dto.getImmediate();
-        this.hourly = dto.getHourly();
-        this.daily = dto.getDaily();
-        this.weekly = dto.getWeekly();
-        this.monthly = dto.getMonthly();
-    }
 }
