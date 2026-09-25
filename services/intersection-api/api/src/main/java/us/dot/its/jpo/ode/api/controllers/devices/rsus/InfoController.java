a6f43d21b2699100c2ea5a7e2bb5df9ac70dbd1b
pmonington <121187998+pmonington@users.noreply.github.com>
2026-09-25 11:18:17 -0600

    Standardize Controller Method Annotations (#369)
    * chore: standardize controller method annotations
    
    * chore: update controller method annotations for CountsController
    
    * chore: remove leading spaces

--- services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/controllers/devices/rsus/InfoController.java
+++ services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/controllers/devices/rsus/InfoController.java
@@ -16,7 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import us.dot.its.jpo.ode.api.models.postgres.dtos.RsuGeoInfoDto;
@@ -36,16 +36,16 @@ public class InfoController {
     private final RsuInfoService rsuInfoService;
 
     @Operation(summary = "Get RSU Geographic Info", description = "Returns a GeoJSON Feature list of all RSUs the user has access to within their specified organisation.")
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    @GetMapping( produces = "application/json")
     @PreAuthorize("@PermissionService.isSuperUser() || @PermissionService.hasRole('USER')")
     @ApiResponses(value = {
-                        @ApiResponse(responseCode = "200", description = "Success", content = @Content(schema = @Schema(description = "GeoJSON Feature array containing one object per RSU"))),
+            @ApiResponse(responseCode = "200", description = "Success", content = @Content(schema = @Schema(description = "GeoJSON Feature array containing one object per RSU"))),
             @ApiResponse(responseCode = "403", description = "Forbidden - Requires membership in the specified organisation"),
     })
-        public ResponseEntity<List<RsuGeoInfoDto>> getRsuInfo(
+    public ResponseEntity<List<RsuGeoInfoDto>> getRsuInfo(
             @RequestHeader(name = "Organization", required = true) String organization) {
         log.debug("GET /devices/rsus/info requested for organisation '{}'", organization);
         List<RsuGeoInfoDto> rsuList = rsuInfoService.getRsuGeoInfoByOrganization(organization);
-                return ResponseEntity.ok(rsuList);
+        return ResponseEntity.ok(rsuList);
     }
 }
